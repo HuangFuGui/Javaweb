dc6c7830b7d6f984f8147faf4a698c05e1fbe974
HuangFuGui <1151650717@qq.com>
2017-05-09 21:26:17 +0800

    Javaweb

--- MyBatisLearning/src/mechanism/Main.java
+++ MyBatisLearning/src/mechanism/Main.java
@@ -13,18 +13,23 @@ import java.io.IOException;
  * Created by huangfugui on 2017/5/8.
  */
 public class Main {
-
     public static void main(String[] args){
 
+        /*
         HelloServiceHandler helloServiceHandler = new HelloServiceHandler();
         HelloService proxy = (HelloService)helloServiceHandler.bind(new HelloServiceImpl());//bind后生成代理对象
         proxy.sayHello("黄复贵");//通过代理对象调用方法
         System.out.println(proxy.getClass().getName());//打印代理对象类对象的符号引用
         createProxyClassFile();//生成代理类的class文件
 
-        /*HelloServiceCgLib helloServiceCgLib = new HelloServiceCgLib();
+        public final class GenerateProxy extends Proxy implements HelloService
+        1. 继承Proxy得到相应的InvocationHandler，从而在代理类中将请求委托，实现路由转发
+        2. 字节码（class对象）生成需要实现相应接口，这也是JDK动态代理需要提供接口的原因（用传入的接口定义新类）
+        */
+
+        HelloServiceCgLib helloServiceCgLib = new HelloServiceCgLib();
         HelloService proxy = (HelloService) helloServiceCgLib.getInstance(new HelloServiceImpl());
-        proxy.sayHello("黄复贵");*/
+        proxy.sayHello("黄复贵");
     }
 
     private static void createProxyClassFile(){
--- MyBatisLearning/src/mechanism/cglibdynamicproxy/HelloServiceCgLib.java
+++ MyBatisLearning/src/mechanism/cglibdynamicproxy/HelloServiceCgLib.java
@@ -11,20 +11,22 @@ import java.lang.reflect.Method;
  */
 public class HelloServiceCgLib implements MethodInterceptor {
 
-    private Object target;
-
     public Object getInstance(Object target){
-        this.target = target;
         Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(this.target.getClass());
+        enhancer.setSuperclass(target.getClass());
         enhancer.setCallback(this);
         return enhancer.create();
     }
+
+    /*
+     CGLib采用了非常底层的字节码技术，其原理是通过字节码技术为一个类创建子类，并在子类中采用方法拦截的技术
+     拦截所有父类方法的调用，顺势织入横切逻辑
+     */
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        System.out.println("CgLib动态代理：我准备说hello");
+        System.out.println("CgLib动态代理：方法调用前");
         Object result = methodProxy.invokeSuper(o,objects);
-        System.out.println("CgLib动态代理：我说过hello了");
+        System.out.println("CgLib动态代理：方法调用后");
         return result;
     }
 }
